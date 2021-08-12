11cba5aa51971e253abc367495b51b4ee618a520
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2021-08-12 12:23:35 -0700

    add user functions, send buff larger than 16 bytes

--- UARTLite_test.c
+++ UARTLite_test.c
@@ -1,60 +1,103 @@
 /*
  * UartLite_test.c: simple test application for the UART Lite core
  *
- * This application does some testing of the UART Lite core configured
- * with baud rate 9600, 8 data bits, no parity.
+ * This application does some testing of the UART Lite core and 
+ * sends the entire english alphabet at 9600 Buad, 8 data bits, no parity,
 *
  */
 
-#include <stdio.h>
 #include "platform.h"
-#include "xil_printf.h"
+#include "xstatus.h"
 #include "xparameters.h"
 #include "xuartlite.h"
 
 
+#define UARTLITE_DEVICE_ID  XPAR_UARTLITE_0_DEVICE_ID
+#define DATA_SIZE  26  // 26 letters in alphabet
+#define BUFF_SIZE  16  // max size of FIFOs
+
+
+int UARTLite_Init_SelfTest(u16 Device_ID);
+
+int SendData(u8 *DataPointer, u8 *DataBufferPointer);
+
+
+// Data to send
+u8 TxData[DATA_SIZE];
+
+// Tx/Rx buffers
+u8 TxBuff[BUFF_SIZE];
+
+// instance of UART Lite core
+XUartLite UartLite0;
+
+
 int main()
 {
-	int UartLite_status;
+	int UartLiteStatus;
+  int TxStatus;
 
-	// Tx/Rx buffers
-	u8 Tx_buff[16];
+  init_platform();
+  
+  UartLiteStatus = UARTLite_Init_SelfTest(UARTLITE_DEVICE_ID);
 
-	int Index;
+  // intialize buffer with alphabet data
+  for(int i = 0; i < DATA_SIZE; i++)
+  {
+    TxData[i] = 65 + i;  // start at decimal 65 which is ASCII 'A'
+  }
+  
+  TxStatus = SendData(TxData, TxBuff);
 
-	// Tx/Rx byte counts
-	unsigned int Tx_bytes_sent;
-
-	// instance of UART Lite core
-	XUartLite UartLite0;
-
-    init_platform();
-
-    // perform initialization tests
-    UartLite_status = XUartLite_Initialize(&UartLite0, XPAR_UARTLITE_0_DEVICE_ID);
-    if (UartLite_status != XST_SUCCESS)
-    {
-    	return XST_FAILURE;
-    }
-
-    // perform self-test tests
-    UartLite_status = XUartLite_SelfTest(&UartLite0);
-    if (UartLite_status != XST_SUCCESS)
-    {
-    	return XST_FAILURE;
-    }
-
-	for (Index = 0; Index < 16; Index++) {
-		Tx_buff[Index] = Index;
-	}
-
-    // send some data
-    // since not in interrupt mode, max data sent per function call is 16 bytes (size of FIFOs)
-    Tx_bytes_sent = XUartLite_Send(&UartLite0, Tx_buff, 16);
-
-
-
-    cleanup_platform();
-    return 0;
+  cleanup_platform();
+  return 0;
 }
+
+
+int UARTLite_Init_SelfTest(u16 Device_ID)
+{
+  int Status;
+  
+  // perform initialization tests
+  Status = XUartLite_Initialize(&UartLite0, Device_ID);
+  if (Status != XST_SUCCESS)
+  {
+    return XST_FAILURE;
+  }
+
+  // perform self-test tests
+  Status = XUartLite_SelfTest(&UartLite0);
+  if (Status != XST_SUCCESS)
+  {
+    return XST_FAILURE;
+  }
+
+  return XST_SUCCESS;
+}
+
+int SendData(u8 *DataPointer, u8 *DataBufferPointer)
+{
+  unsigned int TxBytesSent;
+  
+  // max data sent per function call is 16 bytes (size of FIFOs)
+  while (TxBytesSent != DATA_SIZE)
+  { 
+    // put only 16 bytes of data into Tx_buff
+    for(int i = 0; i < BUFF_SIZE; i++)
+    {
+      DataBufferPointer[i] = DataPointer[TxBytesSent+i];
+    }
+
+  // Send buffer
+    TxBytesSent += XUartLite_Send(&UartLite0, DataBufferPointer, BUFF_SIZE);
+  }
+    
+  // make sure all data was sent out
+  if (TxBytesSent != DATA_SIZE)
+  {
+   return XST_FAILURE;
+  }
+
+  return XST_SUCCESS;
+}  
 
